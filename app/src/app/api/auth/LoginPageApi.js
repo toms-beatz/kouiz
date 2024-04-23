88d1c082f2cca4ec8823d2e29fbeba7599b73c7f
toms-beatz <thomas.mazeau@icloud.com>
2024-04-23 08:07:16 +0000

    Versioning untracked files

--- app/src/app/api/auth/LoginPageApi.js
+++
@@ -1,6 +0,0 @@
-import { axiosInstance } from "../BaseAPI";
-
-export const LoginAPICall = async (email, password) => {
-     const response = await axiosInstance.post('/login', email, password);
-    return response.data;
-}
